1c7b281d913093b6c57e303f1d8fba99865348e9
Vitali Podhornyi <vitalii.podgornii@gmail.com>
2022-09-16 22:46:39 +0200

    did heroku stuff

--- sweater/settings.py
+++ sweater/settings.py
@@ -10,9 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
+from decouple import config
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.conf.global_settings import STATICFILES_STORAGE
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -45,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
 
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestFilesStorage'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -143,7 +151,6 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'coolsite_cache'),
     }
 }
-
 
 CKEDITOR_CONFIGS = {
     'default': {
@@ -210,3 +217,5 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+django_heroku.settings(locals())
